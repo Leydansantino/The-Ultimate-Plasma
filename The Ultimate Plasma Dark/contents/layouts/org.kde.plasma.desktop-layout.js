acb5f9e02b83f2e3da7321fbecce53b332f9d100
Leydansantino <leyvee160@pm.me>
2026-04-11 20:45:42 -0600

    Update org.kde.plasma.desktop-layout.js

--- The Ultimate Plasma Dark/contents/layouts/org.kde.plasma.desktop-layout.js
+++ The Ultimate Plasma Dark/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -3,39 +3,26 @@ var plasma = getApiVersion(1);
 var layout = {
     "desktops": [
         {
-            "applets": [
-            ],
+            "applets": [],
             "config": {
                 "/": {
-                    "ItemGeometries-1440x900": "",
-                    "ItemGeometries-1646x1029": "",
-                    "ItemGeometries-1920x1080": "",
-                    "ItemGeometriesHorizontal": "",
                     "formfactor": "0",
                     "immutability": "1",
                     "lastScreen": "0",
                     "wallpaperplugin": "org.kde.image"
                 },
-                "/ConfigDialog": {
-                    "DialogHeight": "953",
-                    "DialogWidth": "1646"
-                },
                 "/Wallpaper/org.kde.image/General": {
-                    "Image": "/home/leysan/Imágenes/Wallpapers/aurora-borealis-in-mountains-digital-art_3840x2160_xtrafondos.com.jpg",
-                    "SlidePaths": "/home/leysan/.local/share/wallpapers/,/usr/share/wallpapers/"
+                    "Image": "/usr/share/wallpapers/Next/contents/images/3840x2160.png",
+                    "SlidePaths": "/usr/share/wallpapers/"
                 },
                 "/Wallpaper/org.kde.potd/General": {
                     "Provider": "flickr"
-                },
-                "/Wallpaper/org.kde.slideshow/General": {
-                    "SlidePaths": "/home/leysan/Imágenes/Wallpapers/Carrusel/"
                 }
             },
             "wallpaperPlugin": "org.kde.image"
         },
         {
-            "applets": [
-            ],
+            "applets": [],
             "config": {
                 "/": {
                     "formfactor": "0",
@@ -43,13 +30,9 @@ var layout = {
                     "lastScreen": "1",
                     "wallpaperplugin": "org.kde.image"
                 },
-                "/ConfigDialog": {
-                    "DialogHeight": "630",
-                    "DialogWidth": "810"
-                },
                 "/Wallpaper/org.kde.image/General": {
-                    "Image": "/home/leysan/Imágenes/Wallpapers/aurora-borealis-in-mountains-digital-art_3840x2160_xtrafondos.com.jpg",
-                    "SlidePaths": "/home/leysan/.local/share/wallpapers/,/usr/share/wallpapers/"
+                    "Image": "/usr/share/wallpapers/Next/contents/images/3840x2160.png",
+                    "SlidePaths": "/usr/share/wallpapers/"
                 }
             },
             "wallpaperPlugin": "org.kde.image"
@@ -60,35 +43,23 @@ var layout = {
             "alignment": "center",
             "applets": [
                 {
-                    "config": {
-                    },
+                    "config": {},
                     "plugin": "org.kde.plasma.minimizeall"
                 },
                 {
-                    "config": {
-                    },
+                    "config": {},
                     "plugin": "org.kde.plasma.pager"
                 },
                 {
-                    "config": {
-                    },
+                    "config": {},
                     "plugin": "org.kde.plasma.panelspacer"
                 },
                 {
-                    "config": {
-                        "/": {
-                            "popupHeight": "513",
-                            "popupWidth": "402"
-                        }
-                    },
+                    "config": {},
                     "plugin": "org.kde.plasma.notifications"
                 },
                 {
                     "config": {
-                        "/": {
-                            "popupHeight": "497",
-                            "popupWidth": "554"
-                        },
                         "/Appearance": {
                             "autoFontAndSize": "false",
                             "boldText": "true",
@@ -99,39 +70,24 @@ var layout = {
                             "fontSize": "7",
                             "fontStyleName": "SemiBold",
                             "fontWeight": "600"
-                        },
-                        "/ConfigDialog": {
-                            "DialogHeight": "630",
-                            "DialogWidth": "810"
                         }
                     },
                     "plugin": "org.kde.plasma.digitalclock"
                 },
                 {
-                    "config": {
-                    },
+                    "config": {},
                     "plugin": "org.kde.plasma.panelspacer"
                 },
                 {
-                    "config": {
-                        "/": {
-                            "popupHeight": "325",
-                            "popupWidth": "324"
-                        },
-                        "/General": {
-                            "history": "#ffffff,#eff0f1,#edeeef,#191919,#4154cd,#1f2834,#202d3c,#2f1c25,#3b2831"
-                        }
-                    },
+                    "config": {},
                     "plugin": "org.kde.plasma.colorpicker"
                 },
                 {
-                    "config": {
-                    },
+                    "config": {},
                     "plugin": "org.kde.plasma.marginsseparator"
                 },
                 {
-                    "config": {
-                    },
+                    "config": {},
                     "plugin": "org.kde.plasma.systemtray"
                 }
             ],
@@ -141,13 +97,17 @@ var layout = {
                     "immutability": "1",
                     "lastScreen": "0",
                     "wallpaperplugin": "org.kde.image"
+                },
+                "/General": {
+                    "floating": "1",
+                    "adaptiveTransparency": "1"
                 }
             },
             "height": 2,
             "hiding": "normal",
             "location": "top",
-            "maximumLength": 102.875,
-            "minimumLength": 102.875,
+            "maximumLength": 120,
+            "minimumLength": 120,
             "offset": 0
         },
         {
@@ -155,32 +115,19 @@ var layout = {
             "applets": [
                 {
                     "config": {
-                        "/ConfigDialog": {
-                            "DialogHeight": "630",
-                            "DialogWidth": "810"
-                        },
                         "/General": {
                             "iconSpacing": "0",
-                            "launchers": "applications:systemsettings.desktop,applications:org.kde.konsole.desktop,preferred://filemanager,file:///var/lib/flatpak/exports/share/applications/com.brave.Browser.desktop,file:///var/lib/flatpak/exports/share/applications/io.gitlab.librewolf-community.desktop,applications:com.brave.Browser.flextop.brave-cinhimbnkkaeohfgghhklpknlkffjgod-Default.desktop,file:///var/lib/flatpak/exports/share/applications/io.appflowy.AppFlowy.desktop,file:///var/lib/flatpak/exports/share/applications/org.onlyoffice.desktopeditors.desktop,file:///var/lib/flatpak/exports/share/applications/com.valvesoftware.Steam.desktop,file:///var/lib/flatpak/exports/share/applications/io.missioncenter.MissionCenter.desktop,file:///var/lib/flatpak/exports/share/applications/io.github.kolunmi.Bazaar.desktop"
+                            "launchers": "preferred://systemsettings,applications:org.kde.konsole.desktop,preferred://filemanager"
                         }
                     },
                     "plugin": "org.kde.plasma.icontasks"
                 },
                 {
-                    "config": {
-                    },
+                    "config": {},
                     "plugin": "org.kde.plasma.trash"
                 },
                 {
                     "config": {
-                        "/": {
-                            "popupHeight": "593",
-                            "popupWidth": "686"
-                        },
-                        "/ConfigDialog": {
-                            "DialogHeight": "630",
-                            "DialogWidth": "810"
-                        },
                         "/General": {
                             "favoritesPortedToKAstats": "true",
                             "icon": "distributor-logo-white",
@@ -196,18 +143,21 @@ var layout = {
                     "immutability": "1",
                     "lastScreen": "0",
                     "wallpaperplugin": "org.kde.image"
+                },
+                "/General": {
+                    "floating": "1",
+                    "lengthMode": "2"
                 }
             },
-            "height": 5,
+            "height": 8,
             "hiding": "dodgewindows",
             "location": "bottom",
-            "maximumLength": 102.875,
-            "minimumLength": 102.875,
+            "maximumLength": 120,
+            "minimumLength": 0,
             "offset": 0
         }
     ],
     "serializationFormatVersion": "1"
-}
-;
+};
 
 plasma.loadSerializedLayout(layout);
