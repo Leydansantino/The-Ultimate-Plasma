e491e3908803d168ac176efd7c6003c4b80f8f3b
Leydan Santino <leyvee160@pm.me>
2026-08-22 17:25:08 -0600

    Add TUP Look-and-Feel variants (#10)
    * Add TUP Look-and-Feel variants
    
    * Clean Aquamarine wallpaper defaults

---
+++ TUP-Obsidian-Blue/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,155 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "ItemGeometries-1440x900": "",
+                    "ItemGeometries-1646x1029": "",
+                    "ItemGeometries-1920x1080": "",
+                    "ItemGeometriesHorizontal": "",
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "630",
+                    "DialogWidth": "810"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "Image": "@TUP_LOOKANDFEEL_ROOT@/TUP-Obsidian-Blue/contents/wallpapers/obsidian-blue.png"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "center",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "567",
+                            "popupWidth": "678"
+                        },
+                        "/General": {
+                            "favoritesPortedToKAstats": "true",
+                            "icon": "distributor-logo-symbolic"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.kickoff"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.pager"
+                },
+                {
+                    "config": {
+                        "/ConfigDialog": {
+                            "DialogHeight": "630",
+                            "DialogWidth": "810"
+                        },
+                        "/General": {
+                            "launchers": "preferred://filemanager,applications:systemsettings.desktop,applications:org.kde.konsole.desktop"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.icontasks"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.marginsseparator"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "height": 4,
+            "hiding": "dodgewindows",
+            "lengthMode": "fit",
+            "location": "bottom",
+            "maximumLength": 90,
+            "minimumLength": 90,
+            "offset": 0,
+            "opacity": "translucent"
+        },
+        {
+            "alignment": "center",
+            "applets": [
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.appmenu"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.panelspacer"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "451",
+                            "popupWidth": "510"
+                        },
+                        "/Appearance": {
+                            "autoFontAndSize": "false",
+                            "fontFamily": "Inter",
+                            "fontWeight": "400"
+                        },
+                        "/ConfigDialog": {
+                            "DialogHeight": "630",
+                            "DialogWidth": "810"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.panelspacer"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.showdesktop"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "height": 2,
+            "hiding": "normal",
+            "lengthMode": "fill",
+            "location": "top",
+            "maximumLength": 90,
+            "minimumLength": 90,
+            "offset": 0,
+            "opacity": "adaptive"
+        }
+    ],
+    "serializationFormatVersion": "1"
+};
+
+plasma.loadSerializedLayout(layout);
